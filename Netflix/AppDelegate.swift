10c42b0259429e6a01223f43d4f7c321b958a1f7
Maxime Maheo <mmaheo@deezer.com>
2020-02-19 22:27:10 +0100

    Create project

---
+++ Netflix/AppDelegate.swift
@@ -0,0 +1,23 @@
+//
+//  AppDelegate.swift
+//  Netflix
+//
+//  Created by Maxime Maheo on 19/02/2020.
+//  Copyright © 2020 Maxime Maheo. All rights reserved.
+//
+
+import UIKit
+
+@UIApplicationMain
+class AppDelegate: UIResponder, UIApplicationDelegate {
+
+    // MARK: - Properties
+    var window: UIWindow?
+
+    // MARK: - Methods
+    func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
+        
+        return true
+    }
+
+}
